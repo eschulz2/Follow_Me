ab5841bf645edfe36b6f3c0f49c22c3c97695099
Eric Schulz <eschulz2@gmail.com>
2014-06-23 17:06:34 -0500

    adjusted access to users#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root :to => "visitors#new"
   devise_for :users
+  get 'users', to: 'visitors#new'
   resources :users
   resources :visitors
+
+  get 'salt', to: 'users#index'
+
 end
