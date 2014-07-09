3922327039bce0bc4b1acfed64b8e4b81ceca617
Eric Schulz <eschulz2@gmail.com>
2014-07-09 12:49:59 -0500

    charges controller updated

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,7 +4,7 @@ class ChargesController < ApplicationController
 
 	def create
 	  # Amount in cents
-	  @amount = 780
+	  @amount = 100
 
 	  customer = Stripe::Customer.create(
 	    :email => 'example@stripe.com',
